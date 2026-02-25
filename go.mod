163663f8715ee0e3888c23cc7aee492e95c049d5
juanj <juajocervantes16@gmail.com>
2026-02-24 22:38:19 -0600

    MoreNodes

--- go.mod
+++ go.mod
@@ -12,6 +12,10 @@ require (
 )
 
 require (
+	github.com/dlclark/regexp2 v1.11.4 // indirect
+	github.com/dop251/goja v0.0.0-20260219130522-0ba9a5494a59 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
---
+++ nodes/function_node.go
@@ -0,0 +1,99 @@
+package nodes
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"capyflow/api/models"
+
+	"github.com/dop251/goja"
+)
+
+type FunctionNode struct{}
+
+type FunctionParams struct {
+	InputData interface{} `json:"inputData"`
+	Code      string      `json:"code"`
+}
+
+func (n *FunctionNode) Execute(
+	node *models.Node,
+	prev map[string]map[string]interface{},
+) (map[string]interface{}, error) {
+
+	var params FunctionParams
+	if err := json.Unmarshal([]byte(node.Parameters), &params); err != nil {
+		return nil, fmt.Errorf("invalid function parameters: %v", err)
+	}
+
+	// Get JavaScript code
+	code := params.Code
+	if code == "" {
+		return nil, fmt.Errorf("code parameter is required")
+	}
+
+	// Get input data
+	inputData := params.InputData
+
+	// Create JavaScript VM
+	vm := goja.New()
+
+	// Set up console.log
+	consoleObj := vm.NewObject()
+	var logs []string
+	consoleObj.Set("log", func(args ...interface{}) {
+		var parts []string
+		for _, arg := range args {
+			parts = append(parts, fmt.Sprintf("%v", arg))
+		}
+		logs = append(logs, strings.Join(parts, " "))
+	})
+	vm.Set("console", consoleObj)
+
+	// Set input data
+	vm.Set("input", inputData)
+	vm.Set("context", prev)
+
+	// Set common utilities
+	vm.Set("JSON", map[string]interface{}{
+		"parse": func(str string) interface{} {
+			var result interface{}
+			if err := json.Unmarshal([]byte(str), &result); err != nil {
+				return nil
+			}
+			return result
+		},
+		"stringify": func(obj interface{}) string {
+			bytes, err := json.Marshal(obj)
+			if err != nil {
+				return ""
+			}
+			return string(bytes)
+		},
+	})
+
+	// Wrap code in function if not already wrapped
+	if !strings.Contains(code, "return") {
+		code = fmt.Sprintf("(function() { %s })()", code)
+	}
+
+	// Execute the code
+	value, err := vm.RunString(code)
+	if err != nil {
+		return map[string]interface{}{
+			"error":  err.Error(),
+			"logs":   logs,
+			"result": nil,
+		}, fmt.Errorf("JavaScript execution error: %v", err)
+	}
+
+	// Export the result
+	result := value.Export()
+
+	return map[string]interface{}{
+		"result": result,
+		"logs":   logs,
+		"error":  nil,
+	}, nil
+}
